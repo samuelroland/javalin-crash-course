9b1d5c6e29ef7c4bc09b6dcdcd508f1a06b45ced
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-12-20 21:49:26 +0100

    Setup pom.xml, Main.java and AppTest.java

---
+++ src/main/java/org/train/Main.java
@@ -0,0 +1,8 @@
+package org.train;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Javalin crash course");
+
+	}
+}
---
+++ src/test/java/org/train/AppTest.java
@@ -0,0 +1,14 @@
+package org.train;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+	@Test
+	public void testTrue() {
+		assertTrue(true);
+	}
+}
